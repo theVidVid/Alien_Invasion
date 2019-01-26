a54bd6be7268bef7e1fcfaefb079d115ab92b65d
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-01-26 14:46:41 -0800

    Added settings.py file containing the class Settings to hold all settings for Alien Invasion game. Modified alien_invasion.py that imports the newly created class Settings and calls the settings rather than writing them explicitly on the run_game() function.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,19 +2,20 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 """
 The sys module is used to exit the game when the quits.
 The pygame module contains the functionality needed to make a game.
 """
 
 def run_game():
-    # Initializes game and creates a screen object.
+    # Initializes pygame, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings,screen_height))
     pygame.display.set_caption("Alien Invasion")
-    
-    # Set the background color.
-    bg_color = (74, 52, 145)
     
     # Start the main loop for the game.
     while True:
@@ -25,7 +26,7 @@ def run_game():
                 sys.exit()
                 
         # Redraw the screen during each pass through the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         
         # Make the most recently drawn screen visible.
         pygame.display.flip()
