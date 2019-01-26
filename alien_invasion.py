40abe4695c0cf37f4394362d7ca3a5686a27ab04
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-01-26 14:23:55 -0800

    Added new background color, added some docstrings explaining the purpose of the sys and pygame modules.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,19 @@ import sys
 
 import pygame
 
+"""
+The sys module is used to exit the game when the quits.
+The pygame module contains the functionality needed to make a game.
+"""
+
 def run_game():
-    # Initialize game and create a screen object.
+    # Initializes game and creates a screen object.
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    
+    # Set the background color.
+    bg_color = (74, 52, 145)
     
     # Start the main loop for the game.
     while True:
@@ -16,6 +24,9 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
                 
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
+        
         # Make the most recently drawn screen visible.
         pygame.display.flip()
         
