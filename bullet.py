20e8bd0a42b6ebbdb6a9c68e5b94cd190f81a5ef
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-01-31 12:02:36 -0800

    Added draw_bullet() method to draw a bullet to the screen.

--- bullet.py
+++ bullet.py
@@ -28,4 +28,7 @@ class Bullet(Sprite):
         # Update the rect position.
         self.rect.y = self.y
         
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
         
