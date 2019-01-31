290a14f2e80301d134cb8d3c1a34c1ca5e628363
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-01-31 11:20:18 -0800

    Added bullet dimensions, (width, height) and bullet color.

--- settings.py
+++ settings.py
@@ -13,3 +13,6 @@ class Settings():
         
         # Bullet settings
         self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
