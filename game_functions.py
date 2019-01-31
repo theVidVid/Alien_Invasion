77cfa64ad68c1a73c24454d63ccf7272c721fcae
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-01-31 12:52:02 -0800

    Modified game_functions.py file importing Bullet class from bullet module, adding a keypress down event for the spacebar key to fire a bullet; updated check_keydown_events(), check_events(), and update_screen methods adding bullets as a parameter. Changed color of bullet to bright red for better visibility on the screen.

--- game_functions.py
+++ game_functions.py
@@ -2,12 +2,14 @@ import sys
 
 import pygame
 
+from bullet import Bullet
+
 """
 The sys module is used to exit the game when the quits.
 The pygame module contains the functionality needed to make a game.
 """
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events."""
     # Watch for keyboard and mouse events.
     for event in pygame.event.get():
@@ -16,14 +18,14 @@ def check_events(ship):
         
         # KEYDOWN signifies the pressing of a key    
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
             
         # KEYUP signifies the release of a pressed key    
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         # Move the ship to the right.
@@ -32,7 +34,11 @@ def check_keydown_events(event, ship):
     elif event.key == pygame.K_LEFT:
         # Move the ship to the left.
         ship.moving_left = True
-       
+        
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
         
 def check_keyup_events(event, ship):
     """Respond to key releases."""
@@ -45,10 +51,14 @@ def check_keyup_events(event, ship):
         ship.moving_left = False    
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+    
+    # Redraw all bullets behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
         
     # Make the most recently drawn screen visible.
--- settings.py
+++ settings.py
@@ -15,4 +15,4 @@ class Settings():
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = 178, 34, 34
