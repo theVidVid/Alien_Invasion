e7bba014f3a218c172c63887fda3a66bbfc2a7fe
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-01-31 12:14:28 -0800

    Modified alien_invasion.py to import Group from pygame.sprite. Made an instance of Group and called it bullets. Passed bullets as parameters in check_events() method, and update_screen() method.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -17,10 +18,14 @@ def run_game():
     # Make ship instance
     ship = Ship(ai_settings, screen)
     
+    # Make a group to store bullets in.
+    bullets = Group()
+    
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_setting, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
