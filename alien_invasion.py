edf4472038807c591b7d908c0ff37d7ed704647c
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-02-02 13:22:53 -0800

    Moved functionality of limiting bullets fired from alien_invasion.py to game_functions.py, creating a new function named update_bullets(). Moved functionality of creating a new bullet and storing it in bullet group to a new function called fire_bullet() to simplify and streamline code.

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,14 +29,7 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-        
-        # Get rid of bullets that have disappeared.
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        # ~ print(len(bullets))
-        
+        gf.update_bullets(bullets)
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -37,10 +37,16 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
         
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        # Fires a bullet.
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if limit hasn't been reached yet."""
+    # Create a new bullet and add it to bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
         
 def check_keyup_events(event, ship):
@@ -51,7 +57,19 @@ def check_keyup_events(event, ship):
         
     elif event.key == pygame.K_LEFT:
         # The ship stops moving to the left.
-        ship.moving_left = False    
+        ship.moving_left = False
+            
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets"""
+    # Update bullet positions.
+    bullets.update()
+    
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    # ~ print(len(bullets))
 
 
 def update_screen(ai_settings, screen, ship, bullets):
