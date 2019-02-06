e9d33b05f4063873f10fbf28c215fbabd17cdc98
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-02-06 12:17:47 -0800

    Refactored create_fleet() by creating two additional functions: get_number_aliens_x() and create_alien() to clean up original function.

--- game_functions.py
+++ game_functions.py
@@ -93,19 +93,30 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
     
 
+def get_number_aliens_x(ai_settings, alien_width):
+    """Determing the number of aliens that fit in a row."""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x    
+
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    """Create an alien and place it in the row."""
+    alien = Alien(ai_settings, screen)
+    # Spacing between each alien is equal to one alien width.
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number        
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
+
 def create_fleet(ai_settings, screen, aliens):
     """Create a full fleet of aliens."""
     # Create an alien and find the number of aliens in a row.
-    # Spacing between each alien is equal to one alien width.
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+
     
     # Create the first row of aliens
     for alien_number in range(number_aliens_x):
-        # Create an alien and place it in the row.
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, screen, aliens, alien_number)
