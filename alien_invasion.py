ea977f5d9c7db6c3b7ed474baa96de4d0ff5f292
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-02-05 23:25:04 -0800

    Updated alien_invasion.py, replacing a single alien instance and instead creating an aliens group. Updated updated_screen to include aliens instead of alien.

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,20 +21,19 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     
-    # Make ship instance
+    # Make ship instance, a group of bullets, and a group of aliens
     ship = Ship(ai_settings, screen)
-    
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
     
-    # Make alien instance
-    alien = Alien(ai_settings, screen)
+    # Create a fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
     
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
