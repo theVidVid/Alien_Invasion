bdb6d044efd4502dc71149052c7ed979b3d2751e
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-02-08 14:22:07 -0800

    Added class method check_edges() to see whether an alien has hit an edge on the left or right.

--- alien.py
+++ alien.py
@@ -27,6 +27,14 @@ class Alien(Sprite):
     def blitme(self):
         """Draw the alien at its current location."""
         self.screen.blit(self.image, self.rect)
+        
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
     
     def update(self):
         """Move the alien right."""
