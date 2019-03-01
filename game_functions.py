cf7082ee42ba803b672d7a6deef270de5990ebb6
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 13:24:01 -0800

    Added MOUSEBUTTONDOWN event in check_events function.

--- game_functions.py
+++ game_functions.py
@@ -14,9 +14,8 @@ The pygame module contains the functionality needed to make a game.
 """
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Respond to keypresses and mouse events."""
-    # Watch for keyboard and mouse events.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -28,7 +27,17 @@ def check_events(ai_settings, screen, ship, bullets):
         # KEYUP signifies the release of a pressed key    
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+            
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
 
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Start a new game when the player clicks Play button."""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
+            
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
