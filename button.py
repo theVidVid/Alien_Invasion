05cacafb5a24a29fee3a40f45ebb4aa655059bf1
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 13:03:30 -0800

    Corrected typo.

--- button.py
+++ button.py
@@ -11,7 +11,7 @@ class Button():
         self.width, self.height = 200, 50
         self.button_color = (0, 255, 0)
         self.text_color = (255, 255, 255)
-        self.font = pygame.font.sysFont(None, 48)
+        self.font = pygame.font.SysFont(None, 48)
         
         # Build the button's rect object and center it.
         self.rect = pygame.Rect(0, 0, self.width, self.height)
