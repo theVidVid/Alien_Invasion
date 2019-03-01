c5ab8ee767561e1edd769f8d04fa5ae1ffd21f03
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 14:24:35 -0800

    Updated check_play_button() to include stats, ship, aliens, and bullets as parameters and call reset_stats(), aliens.empty(), bullets.empty(), create_fleet() and center_ship()

--- game_functions.py
+++ game_functions.py
@@ -33,11 +33,21 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             check_play_button(stats, play_button, mouse_x, mouse_y)
 
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,        bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play button."""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Reset the game statistics.
+        stats.reset_stats()
         stats.game_active = True
-            
+        
+        # Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
+        
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()    
+
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
