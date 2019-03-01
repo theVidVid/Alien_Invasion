0aafd0fecfffcaeef29ed50fb4694f00ad023e13
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 12:29:49 -0800

    Added draw_button() class method that draws blank button and then draws message.

--- button.py
+++ button.py
@@ -27,3 +27,7 @@ class Button():
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
         
+    def draw_button(self):
+        """ Draw blank button and then draw message."""
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
