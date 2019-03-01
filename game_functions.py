920382a6ec1f210a95dc0bf39e91cfebdf046c61
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 12:53:40 -0800

    Pass play_button as a parameter in update_screen() function, created an if block that sets a condition to make the button visible only if game is inactive.

--- game_functions.py
+++ game_functions.py
@@ -92,7 +92,8 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
         create_fleet(ai_settings, screen, ship, aliens)        
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+        play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -102,6 +103,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+    
+    # Draw the play button if the game is inactive.
+    if not stats.game_active:
+        play_button.draw_button()
         
     # Make the most recently drawn screen visible.
     pygame.display.flip()
