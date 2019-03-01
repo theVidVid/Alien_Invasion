cc54df592cb265515bded1f4f1de58d6ba8f1da0
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 12:41:31 -0800

    Imported Button class from button.py module, created an instance of button, and added that new button instance to gf.update_screen().

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from settings import Settings
 
 from game_stats import GameStats
 
+from button import Button
+
 from ship import Ship
 
 import game_functions as gf
@@ -20,6 +22,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    
+    # Make the Play button
+    play_button = Button(ai_settings, screen, "Play")
     
     # Create an instance to store game statistics
     stats = GameStats(ai_settings)
@@ -41,6 +46,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
             
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+            play_button)
 
 run_game()
