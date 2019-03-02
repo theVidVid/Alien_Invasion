9d64060a24970bbd9d2eee66b99aa76bad8efe1b
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-02 14:54:19 -0800

    Added new function called check_high_scores() that compares current score with high score and if higher, replaces it as the new high score.

--- game_functions.py
+++ game_functions.py
@@ -255,3 +255,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     # Look for aliens hitting the bottom of the screen
     check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
         
+
+def check_high_score(stats, sb):
+    """Check to see if there's a new high score."""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
