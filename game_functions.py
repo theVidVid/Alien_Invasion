6d417871a1e52bca11b2e27607ade281cd7e2a16
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-02 13:14:48 -0800

    Added a for loop in if statement on check_bullet_alien_collisions() function to increase accuracy of scoring.

--- game_functions.py
+++ game_functions.py
@@ -118,8 +118,9 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     
     if collisions:
-        stats.score += ai_settings.alien_points
-        sb.prep_score()
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
     
     if len(aliens) == 0:
         # Destroy existing bullets, speed up game, and create new fleet.
