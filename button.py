4f69797d5a63a6605db3e55506eb9a1eb12a9ab2
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 19:20:50 -0800

    Changed button color to bright orange.

--- button.py
+++ button.py
@@ -9,7 +9,7 @@ class Button():
         
         # Set the dimensions and properties of the button.
         self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
+        self.button_color = (255, 69, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
         
