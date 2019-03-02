f72312e53798f03c91121e0cff74e07d7a914696
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-02 11:36:42 -0800

    Added show_score class method to draw score to the screen.

--- scoreboard.py
+++ scoreboard.py
@@ -27,3 +27,8 @@ class Scoreboard():
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+
+    def show_score(self):
+        """Draw score to the screen."""
+        self.screen.blit(self.score_image, self.score_rect)
+        
