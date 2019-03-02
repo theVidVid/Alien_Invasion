d3662eea6ca5d2c45fc94c3081e80bd822990bc8
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 22:53:52 -0800

    Added the class method prep_score() that turns the score into a rendered image.

--- scoreboard.py
+++ scoreboard.py
@@ -16,3 +16,14 @@ class Scoreboard():
         
         # Prepare the initial score image.
         self.prep_score()
+    
+    def prep_score(self):
+        """Turn the score into a rendered image."""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, self.text_color,
+            self.ai_settings.bg_color)
+            
+        # Display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
