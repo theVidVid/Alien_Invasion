e4106e6e903b03e127ff3c36e69a12f53de43e38
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 20:49:30 -0800

    Created a class method called increase_speed() in the Settings class that increases the speed of the ship, bullet, and alien fleet speed.

--- settings.py
+++ settings.py
@@ -33,3 +33,9 @@ class Settings():
         
         # Fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+        
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
