9f0c9528535d298ca152256347523ee5cca5ed6a
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-01 18:21:18 -0800

    Updated the function definition of check_events() and the check_play_button().

--- game_functions.py
+++ game_functions.py
@@ -14,7 +14,8 @@ The pygame module contains the functionality needed to make a game.
 """
 
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, 
+        bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,10 +31,12 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship,
+                aliens, bullets, mouse_x, mouse_y)
 
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,        bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, 
+        bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play button."""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
         # Reset the game statistics.
