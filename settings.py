3ef3d7e857a6760c5e5f08ec50db93a0574a2b25
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-02 12:03:09 -0800

    Added an integer point value for each alien shot down. Added screenshot of default scoreboard at start of game.

--- settings.py
+++ settings.py
@@ -34,6 +34,9 @@ class Settings():
         # Fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
         
+        # Scoring
+        self.alien_points = 50
+        
     def increase_speed(self):
         """Increase speed settings."""
         self.ship_speed_factor *= self.speedup_scale
