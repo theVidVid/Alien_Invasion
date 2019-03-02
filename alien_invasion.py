087eb7a3f92dcdecd1ed0c2c5ffac25176519dfe
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-03-02 11:42:56 -0800

    Making a class instance of scoreboard and updated the gf.update_screen() function call by adding new scoreboard instance.

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from settings import Settings
 
 from game_stats import GameStats
 
+from scoreboard import Scoreboard
+
 from button import Button
 
 from ship import Ship
@@ -26,8 +28,9 @@ def run_game():
     # Make the Play button
     play_button = Button(ai_settings, screen, "Play")
     
-    # Create an instance to store game statistics
+    # Create an instance to store game statistics and create a scoreboard.
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     
     # Make ship instance, a group of bullets, and a group of aliens
     ship = Ship(ai_settings, screen)
@@ -47,7 +50,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
             
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
-            play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, 
+            bullets, play_button)
 
 run_game()
